08594bddfc7ff48a0ae7da2326628ffa79b58b58
DucThien994 <ndthien994@gmail.com>
2026-08-02 09:29:52 +0700

    add adc.h

--- adc.h
+++ adc.h
@@ -6,6 +6,6 @@
 // kenh ADC1_IN18: noi voi cam bien nhiet do noi (internal temp sensor) / VBAT
 #define ADC_TEMPSENSOR_CHANNEL 18
 
-void ADC1_TempSensor_Init(void);
 uint16_t ADC1_ReadRaw(void);
+void ADC1_TempSensor_Init(void);
 float ADC1_ReadTemperature(void);
