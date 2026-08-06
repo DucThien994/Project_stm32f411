c5bfa2594c1f7a3166778eaf6cabe95f3edfaee0
DucThien994 <ndthien994@gmail.com>
2026-08-06 12:01:00 +0700

    change structure of i2c and spi define register

--- gpio.h
+++ gpio.h
@@ -15,17 +15,24 @@
 #define RCC_APB2ENR (*(volatile uint32_t*)(RCC_BASE_ADDR + 0x44))
 
 // define for gpio register
-#define GPIOA_MODER (*(volatile uint32_t*)(GPIOA_BASE_ADDR))
-#define GPIOB_MODER (*(volatile uint32_t*)(GPIOB_BASE_ADDR))
-#define GPIOC_MODER (*(volatile uint32_t*)(GPIOC_BASE_ADDR))
-#define GPIOD_MODER (*(volatile uint32_t*)(GPIOD_BASE_ADDR))
+#define GPIOA_MODER 	(*(volatile uint32_t*)(GPIOA_BASE_ADDR))
+#define GPIOB_MODER 	(*(volatile uint32_t*)(GPIOB_BASE_ADDR))
+#define GPIOC_MODER 	(*(volatile uint32_t*)(GPIOC_BASE_ADDR))
+#define GPIOD_MODER 	(*(volatile uint32_t*)(GPIOD_BASE_ADDR))
+#define GPIOE_MODER 	(*(volatile uint32_t*)(GPIOD_BASE_ADDR))
 
-#define GPIOA_IDR 	(*(volatile uint32_t*)(GPIOA_BASE_ADDR + 0x14))
 
-#define GPIOB_AFRL 	(*(volatile uint32_t*)(GPIOB_BASE_ADDR + 0x20))
+#define GPIOA_IDR 		(*(volatile uint32_t*)(GPIOA_BASE_ADDR + 0x14))
+#define GPIOA_AFRL 		(*(volatile uint32_t*)(GPIOA_BASE_ADDR + 0x20))
 
-#define ISER0       (*(volatile uint32_t*)(0xE000E100))
-#define ISER1       (*(volatile uint32_t*)(0xE000E104))
+#define GPIOB_AFRL 		(*(volatile uint32_t*)(GPIOB_BASE_ADDR + 0x20))
+#define GPIOB_AFRH 		(*(volatile uint32_t*)(GPIOB_BASE_ADDR + 0x24))
+#define GPIOB_OTYPER 	(*(volatile uint32_t*)(GPIOB_BASE_ADDR + 0x20))
+
+#define GPIOE_ODR 		(*(volatile uint32_t*)(GPIOE_BASE_ADDR))
+
+#define ISER0       	(*(volatile uint32_t*)(0xE000E100))
+#define ISER1       	(*(volatile uint32_t*)(0xE000E104))
 
 typedef enum{
 	// dat dung theo thu tu cua schematic
--- spi.c
+++ spi.c
@@ -11,61 +11,49 @@
 
 void Spi1_Init(void){
     // enable clock 
-    uint32_t* RCC_APB2ENR = (uint32_t*)(RCC_BASE_ADDR + 0x44);
-    *RCC_APB2ENR |= 1 << 12; // enable spi1
-    uint32_t* RCC_AHB1ENR = (uint32_t*)(RCC_BASE_ADDR + 0x30);
-    *RCC_AHB1ENR |= 1 << 0; // enable gpio
-    *RCC_AHB1ENR |= 1 << 4; // enable gpio
+    RCC_APB2ENR |= 1 << 12; // enable spi1
+    RCC_AHB1ENR |= 1 << 0; // enable gpio
+    RCC_AHB1ENR |= 1 << 4; // enable gpio
 
-    uint32_t* GPIOA_MODER   = (uint32_t*)(GPIOA_BASE_ADDR + 0x00);
-    uint32_t* GPIOE_MODER   = (uint32_t*)(GPIOE_BASE_ADDR + 0x00);
-    uint32_t* GPIOE_ODR     = (uint32_t*)(GPIOE_BASE_ADDR + 0x14);
-    uint16_t* SPI1_CR1      = (uint16_t*)(SPI1_BASE_ADDR + 0x00);
-
-    *GPIOA_MODER &= ~(0b111111 << 10);
-    *GPIOA_MODER |= (0b101010 << 10) | (0b10 << 12) | (0b10 << 14); // set alternate function for PA5, PA6, PA7
-    *GPIOE_ODR |= 1 << 3; // set pe3 high (inactive)
+    GPIOA_MODER &= ~(0b111111 << 10);
+    GPIOA_MODER |= (0b101010 << 10) | (0b10 << 12) | (0b10 << 14); // set alternate function for PA5, PA6, PA7
+    GPIOE_ODR |= 1 << 3; // set pe3 high (inactive)
 
     // alternate function for PA5, PA6, PA7
-    uint32_t* GPIOA_AFRL = (uint32_t*)(GPIOA_BASE_ADDR + 0x20);
-    *GPIOA_AFRL &= ~(0xFFF << 20);
-    *GPIOA_AFRL |= (0b0101 << 20) | (0b0101 << 24) | (0b0101 << 28);
+    GPIOA_AFRL &= ~(0xFFF << 20);
+    GPIOA_AFRL |= (0b0101 << 20) | (0b0101 << 24) | (0b0101 << 28);
     
     // pe3 output mode 
-    *GPIOE_MODER &= ~(0b11 << 6);
-    *GPIOE_MODER |= 1 << 6;
+    GPIOE_MODER &= ~(0b11 << 6);
+    GPIOE_MODER |= 1 << 6;
 
     // config spi
-    *SPI1_CR1 |= 1 << 2;
-    *SPI1_CR1 |= (0b11 << 3); // 1MHz 
-    *SPI1_CR1 |= (1 << 8) | (1 << 9); // Internal slave select , Software slave management
-    *SPI1_CR1 |= 1 << 6; // enable SPI
+    SPI1_CR1 |= 1 << 2;
+    SPI1_CR1 |= (0b11 << 3); // 1MHz 
+    SPI1_CR1 |= (1 << 8) | (1 << 9); // Internal slave select , Software slave management
+    SPI1_CR1 |= 1 << 6; // enable SPI
 
 }
 
 char Spi1_Read(char reg_addr){
 
-    uint32_t* GPIOE_ODR = (uint32_t*)(GPIOE_BASE_ADDR + 0x14);
-    *GPIOE_ODR &= ~(1 << 3); // pe3 low : active slave 
+    GPIOE_ODR &= ~(1 << 3); // pe3 low : active slave 
 
-    uint16_t* SPI1_DR = (uint16_t*)(SPI1_BASE_ADDR + 0x0C);
-    uint16_t* SPI1_SR = (uint16_t*)(SPI1_BASE_ADDR + 0x08);
-
-    *SPI1_DR = reg_addr; 
-    while(((*SPI1_SR >> 1) & 1) == 0); // wait TXE 
-    while(((*SPI1_SR >> 0) & 1) == 0); // wait RXNE
-    char dummy_data = *SPI1_DR;
+    SPI1_DR = reg_addr; 
+    while(((SPI1_SR >> 1) & 1) == 0); // wait TXE 
+    while(((SPI1_SR >> 0) & 1) == 0); // wait RXNE
+    char dummy_data = SPI1_DR;
     
-    *SPI1_DR = 0xFF; // gửi dummy byte để cấp xung clock cho slave đẩy data
+    SPI1_DR = 0xFF; // gửi dummy byte để cấp xung clock cho slave đẩy data
     
-    while(((*SPI1_SR >> 1) & 1) == 0); 
-    while(((*SPI1_SR >> 0) & 1) == 0);
-    char data = *SPI1_DR; // real data 
+    while(((SPI1_SR >> 0) & 1) == 0);
+    while(((SPI1_SR >> 1) & 1) == 0); 
+    char data = SPI1_DR; // real data 
 
     // wait BSY = 0
-    while(((*SPI1_SR >> 7) & 1) == 1);
+    while(((SPI1_SR >> 7) & 1) == 1);
     
-    *GPIOE_ODR |= 1 << 3; // set pe3 high (inactive slave)
+    GPIOE_ODR |= 1 << 3; // set pe3 high (inactive slave)
 
     return data;
 
@@ -74,26 +62,23 @@ char Spi1_Read(char reg_addr){
 
 void SPI1_Write(char reg_addr, char reg_val){
 
-    uint32_t* GPIOE_ODR = (uint32_t*)(GPIOE_BASE_ADDR + 0x14);
-	*GPIOE_ODR &= ~(1<<3);	// set PE3 to LOW to active slave
-	uint16_t* SPI1_DR = (uint16_t*)(SPI1_BASE_ADDR + 0x0C);
-	uint16_t* SPI1_SR = (uint16_t*)(SPI1_BASE_ADDR + 0x08);
+	GPIOE_ODR &= ~(1<<3);	// set PE3 to LOW to active slave
 
     // send address 
-	*SPI1_DR = reg_addr;
-	while(((*SPI1_SR >> 1) & 1) == 0);
-	while(((*SPI1_SR >> 0) & 1) == 0);
-    char dummy_data = *SPI1_DR; 
+	SPI1_DR = reg_addr;
+	while(((SPI1_SR >> 1) & 1) == 0);
+	while(((SPI1_SR >> 0) & 1) == 0);
+    char dummy_data = SPI1_DR; 
 
     // send value 
-	*SPI1_DR = reg_val;
-    while(((*SPI1_SR >> 1) & 1) == 0);
-	while(((*SPI1_SR >> 0) & 1) == 0);
-	dummy_data = *SPI1_DR;
+	SPI1_DR = reg_val;
+    while(((SPI1_SR >> 1) & 1) == 0);
+	while(((SPI1_SR >> 0) & 1) == 0);
+	dummy_data = SPI1_DR;
 
     // wait SPI free
-	while(((*SPI1_SR >> 7)&1)==1);
+	while(((SPI1_SR >> 7)&1)==1);
 
-	*GPIOE_ODR |= (1<<3);	// set PE3 to HIGH to inactive slave
+	GPIOE_ODR |= (1<<3);	// set PE3 to HIGH to inactive slave
 
 }
