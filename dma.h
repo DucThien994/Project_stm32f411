3e52f3da0defc97c7e404ada7398a39797e3e408
Ducthien994 <nthien994@gmail.com>
2026-08-21 12:42:11 +0700

    fix decleration

--- dma.h
+++ dma.h
@@ -6,7 +6,6 @@
 
 #define DMA1_BASE_ADDR (*(volatile uint32_t *)(0x40026000))
 #define DMA2_BASE_ADDR (*(volatile uint32_t *)(0x40026400))
-#define DMA2_BASE_ADDR (*(volatile uint32_t *)(0x40026400))
 
 #define DMA2_HISR (*(volatile uint32_t *)(0x40026400))
 #define DMA2_HIFCR (*(volatile uint32_t *)(0x40026400))
@@ -15,6 +14,6 @@
 #define DMA2_S7M0AR (*(volatile uint32_t *)(0x40026400 + 0x10 + 0X18 * 7))
 
 void DMA2_USART1_TX_Init(void);
-void DMA2_USART1_TX_Send(uint8_t *src_addr, uint16_t lenght);
+void DMA2_USART1_TX_Send(uint8_t *src_addr, uint16_t length);
 
 #endif
